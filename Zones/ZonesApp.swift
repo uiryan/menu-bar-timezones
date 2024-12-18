69e18cff3b722298b973df30a471b8eb5064ed08
Ryan Meyers <ryanmeyers.co@gmail.com>
2024-12-18 14:00:27 -0500

    Initial Commit

---
+++ Zones/ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonesApp.swift
+//  Zones
+//
+//  Created by Ryan Meyers on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZonesTests/ZonesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZonesTests.swift
+//  ZonesTests
+//
+//  Created by Ryan Meyers on 12/18/24.
+//
+
+import Testing
+@testable import Zones
+
+struct ZonesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
